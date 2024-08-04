fa66247e949bb5ed42ce7f15f117c32b8c2ceb6c
beshoynady <bishoin2017@gmail.com>
2024-08-04 03:02:16 +0300

    ManagLayout

--- client/src/screens/management/manag.component/managerdash/ManagerDash.jsx
+++ client/src/screens/management/manag.component/managerdash/ManagerDash.jsx
@@ -620,7 +620,7 @@ const ManagerDash = () => {
   }, [update]);
 
   useEffect(() => {
-    getCashRegistersByEmployee(employeeLoginInfo.id)
+    getCashRegistersByEmployee(employeeLoginInfo&&employeeLoginInfo.id)
   }, []);
 
 
