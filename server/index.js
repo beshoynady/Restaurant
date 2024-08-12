d77e5afbec1607fb8120297b757edc1b0e62d1d4
beshoynady <bishoin2017@gmail.com>
2024-08-12 14:13:00 +0300

    server

--- server/index.js
+++ server/index.js
@@ -145,6 +145,6 @@ io.on('connect', (socket) => {
 const port = process.env.PORT || 8000;
 
 // Start the server
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
