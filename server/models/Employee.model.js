789083c3753797dbda2b2c37d09b096f490c0c82
beshoynady <bishoin2017@gmail.com>
2024-08-16 19:44:06 +0300

    email

--- server/models/Employee.model.js
+++ server/models/Employee.model.js
@@ -58,8 +58,6 @@ const employeeSchema = new mongoose.Schema(
     },
     email: {
       type: String,
-      unique: true,
-      sparse: true,
       maxlength: 100,
       trim: true,
       validate: {
