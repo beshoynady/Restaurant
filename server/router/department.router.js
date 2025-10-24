3665cd58b693623e4088a4f9032e9176fd77b32a
beshoynady <bishoin2017@gmail.com>
2025-10-24 17:13:24 +0300

    department.controller

--- server/router/department.router.js
+++ server/router/department.router.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const {
-  createdepartment,
-  getAlldepartments,
-  getdepartmentById,
-  updatedepartment,
-  deletedepartment,
-} = require("../controllers/department.controller.js");
+  createDepartment,
+  getAllDepartments,
+  getDepartmentById,
+  updateDepartment,
+  deleteDepartment,
+} = require("../controllers/department.controller");
 
 const { authenticateToken } = require("../middlewares/authenticate");
 const checkSubscription = require("../middlewares/checkSubscription");
 
 router
   .route("/")
-  .post(authenticateToken, checkSubscription, createdepartment)
-  .get(authenticateToken, checkSubscription, getAlldepartments);
+  .post(authenticateToken, checkSubscription, createDepartment)
+  .get(authenticateToken, checkSubscription, getAllDepartments);
 
 router
   .route("/:id")
-  .get(authenticateToken, checkSubscription, getdepartmentById)
-  .put(authenticateToken, checkSubscription, updatedepartment)
-  .delete(authenticateToken, checkSubscription, deletedepartment);
+  .get(authenticateToken, checkSubscription, getDepartmentById)
+  .put(authenticateToken, checkSubscription, updateDepartment)
+  .delete(authenticateToken, checkSubscription, deleteDepartment);
 
 module.exports = router;
